aa289120283c8d98e0f2f83b1e1ce429781a6c45
Jonathan Frederick <jrfreder@gmail.com>
2015-09-09 14:19:40 -0400

    vector_add and vector_sub passing

--- linear_algebra.py
+++ linear_algebra.py
@@ -6,3 +6,20 @@ def shape(thing):
         return len(thing), len(thing[1])
     except:
         return (len(thing), )
+
+
+def same_shape(a, b):
+    return shape(a) == shape(b)
+
+
+def vector_add(a, b):
+    if same_shape(a,b):
+        return [a[x]+b[x] for x in range(len(a))]
+    else:
+        raise ShapeException("Vectors must be the same size")
+
+def vector_sub(a, b):
+    if same_shape(a,b):
+        return [a[x]-b[x] for x in range(len(a))]
+    else:
+        raise ShapeException("Vectors must be the same size")
