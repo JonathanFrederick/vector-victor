99aecbcd23ac7f6ff8835858ebb8fe6acb5039b3
Jonathan Frederick <jrfreder@gmail.com>
2015-09-10 10:19:06 -0400

    tidying and better fixing vector_sum with *args

--- linear_algebra.py
+++ linear_algebra.py
@@ -26,41 +26,21 @@ def vector_sub(a, b):
     return [a[x]-b[x] for x in range(len(a))]
 
 
-def vector_sum(*args): #TODO
-    total = args[0]
-    for vect in args[1:]:
-        total = vector_add(total, vect)
-        print(total)
-    return total
+def vector_sum(*args):
+    # total = args[0]
+    # for vect in args[1:]:
+    #     total = vector_add(total, vect)
+    #     print(total)
+    # return total
 
+    if False in [shape(args[0]) == shape(x) for x in args]:
+        raise ShapeException("Vectors must be the same size")
+    return [sum(x) for x in zip(*args)]
 
-    #return vector_add(args[0], vector_sum([x for x in args[1:] if shape_diff_exception(args[0], x)]))
-
-    #return [vector_add(args[0],  x) for x in args[1:] if shape_diff_exception(args[0], args[x])]
-
-    # if args = None:
-    #     return total
-    # else:
-    #     return vector_add(vector_sum)
-
-    # print(args)
-    # print([shape(args[0]) for x in args])
-    # print([shape(x) for x in args])
-    # print([shape(args[0]) == shape(x) for x in args])
-    # if False in [shape(args[0]) == shape(x) for x in args]:
-    #     raise ShapeException("Vectors must be the same size")
-    # return [sum([y[x] for y in args]) for x in range(len(args))]
-
-
-    # if isinstance(args, zip):
-    #     print("is zip")
     # if len(args) == 2:
-    #     shape_diff_exception(args[0], args[1])
     #     return vector_add(args[0], args[1])
     # else:
-    #     ret = args
-    #     print(ret)
-    #     return vector_add(args[0], vector_sum((x for x in args)))
+    #     return vector_add(args[0], vector_sum(*args[1:]))
 
 
 def dot(a, b):
@@ -90,3 +70,11 @@ def matrix_row(matr, row):
 
 def matrix_col(matr, col):
     return [row[col] for row in matr]
+
+
+def matrix_scalar_multiply(matr, scal):
+    return [vector_multiply(vect, scal) for vect in matr]
+
+
+def matrix_vector_multiply(matr, vect):
+    return [dot(x, vect) for x in matr]
